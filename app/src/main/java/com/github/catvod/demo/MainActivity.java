3bec18a50aaa1fe4e372c20cbd018dd3cbf7b513
asters1 <gj744559140@163.com>
2022-12-02 10:45:11 +0000

    测试

--- app/src/main/java/com/github/catvod/demo/MainActivity.java
+++ app/src/main/java/com/github/catvod/demo/MainActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 
 
-import com.github.catvod.spider.HOME;
-import com.github.catvod.spider.LiteApple;
 
 
 public class MainActivity extends Activity {
@@ -17,18 +15,6 @@ public class MainActivity extends Activity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                HOME source = new HOME();
-                //LiteApple source = new LiteApple();
-                source.init(MainActivity.this, "https://vipmv.co/xgapp.php/v1/");
-               // source.init(MainActivity.this);
-                String homeContent_json  = source.homeContent(true);
-                String categoryContent_json = source.categoryContent("少儿/","",true,null);
-               // source.detailContent(categoryContent_json.)
-                //类型json
-                System.out.println("=======homeContent_json--->："+homeContent_json);
-                //一个类型下面的所有video，json
-                System.out.println("=======homeVideoContent_json--->："+categoryContent_json);
-
             }
         }).start();
     }
